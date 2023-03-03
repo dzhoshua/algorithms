1e8b52d455f45dd29971bf6ccd98cf181fedc394
злата <118795314+dzhoshua@users.noreply.github.com>
2023-03-04 00:16:41 +0800

    add sum

--- 25-02/main.cpp
+++ 25-02/main.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
+#include <fstream>
 #include <filesystem>
 #include <string>
 using namespace std;
 //g++ -std=c++17 .\main.cpp
 
+int sum = 0;
+
 string func(string path) {
 	for (const auto& entry : std::filesystem::directory_iterator(path)) {
 		cout << entry.path() << endl;
-		cout << entry.is_regular_file() << endl;
+		if (entry.is_regular_file()) {
+			ifstream in(entry.path().string());
+			if (!in.is_open()) return "error";
+			int num;
+			while (!in.eof()) {
+				in >> num;
+				sum += num;
+			}
+			in.close();
+		}
 		if (entry.is_directory()) {
 			func(entry.path().string());
 			cout << entry.path() / "out" << endl;
@@ -19,5 +31,7 @@ string func(string path) {
 int main(void) {
 	string path = "data";
 	func(path);
+	cout << "SUM = "<<sum<<endl;
+	cout << "END";
 	return 0;
 }
